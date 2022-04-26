63fa3cd0564c6dae442a2dd4f35c0d768c2eced3
ogulcansarioglu <ogulcansarioglu1@gmail.com>
2022-04-27 00:16:25 +0100

    load and display tasks

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -2,8 +2,19 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+import ie.tudublin.task;
+import processing.data.Table;
+import processing.data.TableRow;
+
+
+
+
 public class Gantt extends PApplet
 {	
+	ArrayList<task> tasks = new ArrayList<task>();
+	
+
 	
 	public void settings()
 	{
@@ -12,12 +23,21 @@ public class Gantt extends PApplet
 
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv", "header");
+		for(TableRow r: table.rows()) 
+	
+		{
+
+			task ts = new task(r);
+			tasks.add(ts);
+		}
 	}
 
 	public void printTasks()
 	{
-		
+		for(task t: tasks) {
+			System.out.println(t);
+		}
 	}
 	
 	public void mousePressed()
@@ -34,10 +54,14 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
 	{			
 		background(0);
+	
+		
 	}
 }
